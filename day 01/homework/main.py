cda231cf00b718ff234a4615a2e40f8516acbae4
ioane2013 <ioanenonasha@gmail.com>
2025-02-25 15:36:45 +0400

    first commit

---
+++ day 01/homework/main.py
@@ -0,0 +1,23 @@
+name = "ioane"
+surname = "nonashvili"
+age = 11
+height = 150
+
+
+print("me var "+ name +" "+ surname + " "+str(age) +" wlis simagle "+ str(height) +" santimetri")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
